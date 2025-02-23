d5288f22c7c3c975cb622e98fe0da2a2d5737499
usamaElgendy <48976562+usamaaelgendy@users.noreply.github.com>
2025-02-23 07:11:46 +0200

    Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_state_management_bloc_cubit/controllers/task_bloc.dart';
-import 'package:flutter_state_management_bloc_cubit/controllers/task_event.dart';
+import 'package:flutter_state_management_bloc_cubit/controllers/login_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,69 +17,36 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key, required this.title});
-
-  final String title;
-  final TextEditingController controller = TextEditingController();
+class LoginPage extends StatelessWidget {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: BlocProvider(
-        create: (context) => TaskBloc(),
-        child: BlocBuilder<TaskBloc, TaskState>(
-          builder: (context, state) {
-            final controllerCubit = context.read<TaskBloc>();
-            return Column(
-              children: [
-                TextField(
-                  controller: controller,
-                  decoration: const InputDecoration(hintText: 'Enter a task'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (controller.text.isEmpty) return;
-                    controllerCubit.add(AddTaskEvent(controller.text));
-                    controller.clear();
-                  },
-                  child: const Text("Add Task"),
-                ),
-                Expanded(
-                    child: ListView.builder(
-                  itemCount: state.tasksList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(
-                        state.tasksList[index].title,
-                      ),
-                      leading: Checkbox(
-                        value: state.tasksList[index].isCompleted,
-                        onChanged: (value) {
-                          controllerCubit.add(ToggleTaskEvent(state.tasksList[index].id));
-                        },
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {
-                          controllerCubit.add(RemoveTaskEvent(state.tasksList[index].id));
-                        },
-                        icon: const Icon(Icons.delete),
-                      ),
-                    );
-                  },
-                ))
-              ],
-            );
-          },
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(controller: usernameController, decoration: const InputDecoration(labelText: "Username")),
+            TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: "Password"),
+                obscureText: true),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                context.read<LoginBloc>().login(usernameController.text, passwordController.text);
+              },
+              child: const Text("Login"),
+            ),
+          ],
         ),
       ),
     );
